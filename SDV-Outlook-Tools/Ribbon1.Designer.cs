27a1a21b47c736bd1382cddafbd8ad63fa55db62
Daniel Thaller <daniel_thaller@web.de>
2020-09-17 12:25:13 +0200

    Erstbefuellung Repo

---
+++ SDV-Outlook-Tools/Ribbon1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SDV_Outlook_Tools
+{
+    partial class rb_sdv_outlook_tools : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public rb_sdv_outlook_tools()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">"true", wenn verwaltete Ressourcen gelöscht werden sollen, andernfalls "false".</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rb_sdv_outlook_tools));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btn_RemoveAttachments = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "SDV Outlook Tools";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btn_RemoveAttachments);
+            this.group1.Name = "group1";
+            // 
+            // btn_RemoveAttachments
+            // 
+            this.btn_RemoveAttachments.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btn_RemoveAttachments.Image = ((System.Drawing.Image)(resources.GetObject("btn_RemoveAttachments.Image")));
+            this.btn_RemoveAttachments.Label = "Remove Attachments";
+            this.btn_RemoveAttachments.Name = "btn_RemoveAttachments";
+            this.btn_RemoveAttachments.ScreenTip = "entfernt alle Mailanhänge von allen gelesenen E-Mails älter 90 Tage.";
+            this.btn_RemoveAttachments.ShowImage = true;
+            this.btn_RemoveAttachments.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_RemoveAttachments_Click);
+            // 
+            // rb_sdv_outlook_tools
+            // 
+            this.Name = "rb_sdv_outlook_tools";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_RemoveAttachments;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal rb_sdv_outlook_tools Ribbon1
+        {
+            get { return this.GetRibbon<rb_sdv_outlook_tools>(); }
+        }
+    }
+}
---
+++ SDV-Outlook-Tools/Ribbon1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop;
+using System.IO;
+//https://github.com/matthewproctor/OutlookAttachmentExtractor/blob/master/Program.cs
+namespace SDV_Outlook_Tools
+{
+    public partial class rb_sdv_outlook_tools
+    {
+        static int totalfilesize = 0;
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+        }
+
+        private void btn_RemoveAttachments_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                RemoveAttachments();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Fehler im Programmablauf", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void RemoveAttachments()
+        {
+            try
+            {
+                DialogResult result1 = MessageBox.Show("Möchten Sie wirklich die Änhange aller Mails älterer als xx Tage entfernen?", "Entfernen der Änhänge", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                //Remove Attachments from Mails older than xx Days
+                if (result1 == DialogResult.Yes)
+                {
+                    string pathToSave = getSaveFolder();
+                    if (pathToSave != "0")
+                    {
+//                        Microsoft.Office.Interop.Outlook.Application Application = new Microsoft.Office.Interop.Outlook.Application();
+//                        Microsoft.Office.Interop.Outlook.Accounts accounts = Application.Session.Accounts;
+//                        foreach (Microsoft.Office.Interop.Outlook.Account account in accounts)
+//                        {
+//                            Console.WriteLine(account.DisplayName);
+//                            //EnumerateFolders(account.Folder);
+//                            EnumerateFoldersInDefaultStore(pathToSave);
+//                        }
+                        EnumerateFoldersInDefaultStore(pathToSave);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Fehler im Programmablauf", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        public static string getSaveFolder()
+        {
+            try
+            {
+                FolderBrowserDialog folderDlg = new FolderBrowserDialog
+                {
+                    ShowNewFolderButton = true
+                };
+                DialogResult result = folderDlg.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    return folderDlg.SelectedPath;
+                }
+                else
+                {
+                    return "0";
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "0";
+            }
+        }
+
+        static void EnumerateFoldersInDefaultStore(string pathToSaveFile)
+        {
+            Microsoft.Office.Interop.Outlook.Application Application = new Microsoft.Office.Interop.Outlook.Application();
+            Microsoft.Office.Interop.Outlook.Folder root = Application.Session.DefaultStore.GetRootFolder() as Microsoft.Office.Interop.Outlook.Folder;
+            EnumerateFolders(root, pathToSaveFile);
+        }
+
+        static void EnumerateFolders(Microsoft.Office.Interop.Outlook.Folder folder, string pathToSaveFile)
+        {
+            Microsoft.Office.Interop.Outlook.Folders childFolders = folder.Folders;
+            if (childFolders.Count > 0)
+            {
+                foreach (Microsoft.Office.Interop.Outlook.Folder childFolder in childFolders)
+                {
+                    // We only want Inbox folders - ignore Contacts and others
+                    if (childFolder.FolderPath.Contains("Inbox"))
+                    {
+                        MessageBox.Show(childFolder.FolderPath);
+                        // Call EnumerateFolders using childFolder, to see if there are any sub-folders within this one
+                        EnumerateFolders(childFolder, pathToSaveFile);
+                    }
+                }
+            }
+            MessageBox.Show("Checking in " + folder.FolderPath);
+            IterateMessages(folder, pathToSaveFile);
+        }
+
+        static void IterateMessages(Microsoft.Office.Interop.Outlook.Folder folder, string basePath)
+        {
+            var fi = folder.Items.Restrict("[Unread] = true");
+            if (fi != null)
+            {
+                foreach (Object item in fi)
+                {
+                    Microsoft.Office.Interop.Outlook.MailItem mi = (Microsoft.Office.Interop.Outlook.MailItem)item;
+                    var attachments = mi.Attachments;
+                    if (attachments.Count != 0)
+                    {
+                        // Create a directory to store the attachment 
+                        if (!Directory.Exists(basePath + folder.FolderPath))
+                        {
+                            Directory.CreateDirectory(basePath + folder.FolderPath);
+                        }
+
+                        for (int i = 1; i <= mi.Attachments.Count; i++)
+                        {
+                            var fn = mi.Attachments[i].FileName.ToLower();
+                            //check wither any of the strings in the extensionsArray are contained within the filename
+//                            if (extensionsArray.Any(fn.Contains))
+//                            {
+
+                                // Create a further sub-folder for the sender
+                                if (!Directory.Exists(basePath + folder.FolderPath + @"\" + mi.Sender.Address))
+                                {
+                                    Directory.CreateDirectory(basePath + folder.FolderPath + @"\" + mi.Sender.Address);
+                                }
+                                //totalfilesize = totalfilesize + mi.Attachments[i].Size;
+                                if (!File.Exists(basePath + folder.FolderPath + @"\" + mi.Sender.Address + @"\" + mi.Attachments[i].FileName))
+                                {
+                                    Console.WriteLine("Saving " + mi.Attachments[i].FileName);
+                                    mi.Attachments[i].SaveAsFile(basePath + folder.FolderPath + @"\" + mi.Sender.Address + @"\" + mi.Attachments[i].FileName);
+                                    mi.Body = mi.Body + "Anhange nach " + basePath + folder.FolderPath + @"\" + mi.Sender.Address + @"\" + mi.Attachments[i].FileName+" gespeichert.";
+                                   //mi.Attachments[i].Delete();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Already saved " + mi.Attachments[i].FileName);
+                                }
+  //                          }
+
+
+
+
+
+
+                                //mi.Attachments[i].SaveAsFile(pathToSaveFile);
+                                //mi.Attachments[i].Delete();
+                                Console.WriteLine("Subject: " +mi.Subject+" Attachment: " + mi.Attachments[i].FileName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
